595587920d57776278280bcb72a7fcae2cf24151
Mayhem-XD <leahblo1994@gmail.com>
2023-03-20 11:06:52 +0900

    함수

---
+++ 02.JavaScript/ch05함수/05.timer.js
@@ -0,0 +1,15 @@
+// 타이머 내장 함수
+setTimeout(function(){
+    console.log('3초 경과');
+},3000);  //ms
+
+const threeSeconds = function(){console.log('3초 경과');}
+setTimeout(threeSeconds,3000);
+
+const si=setInterval(function(){
+    console.log(new Date);
+},1000);
+
+setTimeout(function(){
+    clearInterval(si);
+},5100);
