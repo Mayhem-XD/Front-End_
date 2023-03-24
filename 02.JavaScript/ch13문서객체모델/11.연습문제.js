8da329e94d70c6885c5801e63511dd5fe19843e3
Mayhem-XD <leahblo1994@gmail.com>
2023-03-24 09:32:52 +0900

    ch13_연습문제_수정

--- 02.JavaScript/ch13문서객체모델/11.연습문제.js
+++ 02.JavaScript/ch13문서객체모델/11.연습문제.js
@@ -1,158 +1,78 @@
-function ans1()
-{
+function ans1() {
     let num1 = Number(document.getElementById('num1').value);
     let num2 = Number(document.getElementById('num2').value);
-    if(num1<1||num1>20||num2<1||num2>30||(num2-num1)<4)
-    {
-        alert('입력값의 범위를 확인하고 다시 입력하세요');
+    if (num1 < 0 || num1 > 20 || num2 < 10 || num2 > 30 || num2 - num1 < 4) {
+        alert('입력값의 범위를 확인하고 다시 입력하세요.');
         return;
     }
-    let powerArr =[];
-    for (let i=num1;i<=num2;i++)
-    {
-        powerArr.push(Math.pow(2, i));
-    }
-    let resultStr='';
-    //1줄씩 <br>삽입
-    // for (let power of powerArr)     
-    // {
-    //     resultStr+=power + '<br>'
-    // }
-    //4개 출력후 줄바꿈
-    for (let i =0;i<powerArr.length;i++)
-    {
-        if ((i+1)%4==0)
-        {
-            resultStr += powerArr[i] +'<br>';
-        }
+    let powerArr = [];
+    for (let i = num1; i <= num2; i++)
+        powerArr.push(2 ** i);
+    let resultStr = '';
+    /* for (let power of powerArr)
+        resultStr += power + '<br>'; */
+    for (let i = 0; i < powerArr.length; i++) {
+        if ((i + 1) % 4 == 0)
+            resultStr += powerArr[i] + '<br>';
         else
-        {
-            resultStr += powerArr[i] + ',';
-        }
+            resultStr += powerArr[i] + ', ';
     }
-    document.getElementById('result1').innerHTML=resultStr;
+    document.getElementById('result1').innerHTML = resultStr;
 }
 
-function ans2()
-{
+function ans2() {
     let text = document.getElementById('sentence').value;
+    let search = document.getElementById('search').value.trim();    // 찾을 단어 주위의 공백을 없애 줌
     let noPunct = text.replace(/[',.]/g, '').replace(/[-\n]/g, ' ').toLowerCase();
-    let words=noPunct.split(' ');
-    let count=0;
-    for (let word of words)
-    {
-        if(word=='the')
-        {
+    console.log(noPunct);
+    let words = noPunct.split(' ');
+    let count = 0;
+    for (let word of words) {
+        if (word == search)
             count++;
-        }
     }
-    document.getElementById('result2').innerHTML=`the의 갯수는 ${count} 입니다.`;
-
+    document.getElementById('result2').innerHTML = `${search}의 갯수는 ${count} 입니다.`;
 }
 
-function ans3()
-{
+function ans3() {
     let priceStr = document.getElementById('price').value;
-    let prices = priceStr.split(';');       //prices는 배열, element는 string
-    // let newPrices=[];
-    // for (let price of prices)
-    // {
-    //     newPrices.push(Number(price));
-    // }
-    //각각의 값에 대해 parseInt 한 값 보내기
-    let newPrices=prices.map((x)=>parseInt(x));
-    newPrices.sort((a,b)=>(b-a));
-    let resultStr='';
-    for (let i =0;i<newPrices.length;i++)
-    {
-        if ((i+1)%4==0)
-        {
-            resultStr += newPrices[i] +'<br>';
-        }
-        else
-        {
-            resultStr += newPrices[i] + ',';
-        }
-    }
-
-
-    document.getElementById('result3').innerHTML=`prices: ${resultStr}`;
-
+    let prices = priceStr.split(';');       // prices는 배열, element는 string
+    // prices 의 각 요소를 정수로 변환
+    let newPrices = [];
+    for (let price of prices)
+        newPrices.push(parseInt(price));
+    // let newPrices = prices.map(function(x) {return parseInt(x)});
+    // let newPrices = prices.map(x => parseInt(x));
+    newPrices.sort(function(a, b) {
+        return b - a;               // 내림차순 정렬
+    });
+    let resultStr = '';
+    for (let element of newPrices)
+        resultStr += element + '<br>';
+    document.getElementById('result3').innerHTML = resultStr;
 }
 
-
-// 시계
-/* function twoDigit(num) {
-    return (num < 10) ? '0' + num : String(num);
-}
-function myDatetime(date)
-{
-    return (`${date.getFullYear()}-${twoDigit(date.getMonth()+1)}-${twoDigit(date.getDate())}`+
-    `-${twoDigit(date.getHours())}:${twoDigit(date.getMinutes())}:${twoDigit(date.getSeconds())}`)
-} */
-
-
-/* window.onload=function()
-{
-    setInterval(function(){
-        const now = new Date();
-        const week =['일','월','화','수','목','금','토'];
-        const hour1 =document.getElementById('hour1');
-        const hour2 =document.getElementById('hour2');
-        const min1 =document.getElementById('min1');
-        const min2 =document.getElementById('min2');
-        const sec1 =document.getElementById('sec1');
-        const sec2 =document.getElementById('sec2');
-        //
-        const dateStr = myDatetime(now).substring(2,10);    //23-03-21
-        const timeStr = myDatetime(now).substring(11);
-        //
-        const hourNow =now.getHours();
-        const minNow =now.getMinutes();
-        const secNow =now.getSeconds();
-        //  hh = 일의자리 ex) 23시 -> 3 /h = 십의자리 ex 23-hh(23) -> 20 을 10으로 나눔 -> 2
-        let hh=hourNow%10 ,h, mm=minNow%10 ,m, ss=secNow%10, s;                                           
-        hourNow>10 ? h=((hourNow-hh)/10) : h=0;                         
-        minNow>10 ? m=((minNow-mm)/10) : m=0;                         
-        secNow>10 ? s=((secNow-ss)/10) : s=0;                         
-        hour1.src=`../../00.numbers_img/${h}.svg`;
-        hour2.src=`../../00.numbers_img/${hh}.svg`;
-        min1.src=`../../00.numbers_img/${m}.svg`;
-        min2.src=`../../00.numbers_img/${mm}.svg`;
-        sec1.src=`../../00.numbers_img/${s}.svg`;
-        sec2.src=`../../00.numbers_img/${ss}.svg`;
-        
-        //let week='일월화수목금토'.split('');
-        //dateStr+='('+week[now.getDay()]+')';
-        document.getElementById('date').innerHTML=`${dateStr}  (${week[now.getDay()]}) ` ;
-        document.getElementById('time').innerHTML=timeStr;
-        //
-    },1000);
-    
-} */
 function twoDigit(num) {
     return (num < 10) ? '0' + num : String(num);
 }
-function myDatetime(date)
-{
-    return (`${date.getFullYear()}-${twoDigit(date.getMonth()+1)}-${twoDigit(date.getDate())}`+
-    `-${twoDigit(date.getHours())}:${twoDigit(date.getMinutes())}:${twoDigit(date.getSeconds())}`)
+function myDatetime(date) {
+    return `${date.getFullYear()}-${twoDigit(date.getMonth()+1)}-${twoDigit(date.getDate())} ` +
+        `${twoDigit(date.getHours())}:${twoDigit(date.getMinutes())}:${twoDigit(date.getSeconds())}`;
 }
-window.onload(function(){
-    setInterval(function(){
+window.onload = function() {
+    setInterval(function() {
         const now = new Date();
-        let week='일월화수목금토'.split('');
-        const dateStr = myDatetime(now).substring(2,10); 
-        dateStr+='('+week[now.getDay()]+')';
-        const timeStr = myDatetime(now).substring(11,19)
-
-
-        document.getElementById('h1').src=`../../00.numbers_img/${timeStr[0]}.svg`
-        document.getElementById('h2').src=`../../00.numbers_img/${timeStr[0]}.svg`
-        document.getElementById('m1').src=`../../00.numbers_img/${timeStr[0]}.svg`
-        document.getElementById('m2').src=`../../00.numbers_img/${timeStr[0]}.svg`
-        document.getElementById('s1').src=`../../00.numbers_img/${timeStr[0]}.svg`
-        document.getElementById('s2').src=`../../00.numbers_img/${timeStr[0]}.svg`
+        const week = '일월화수목금토'.split('');            // ['일','월', ..., '토']
+        let dateStr = myDatetime(now).substring(2, 10);     // 23-03-23
+        dateStr += '(' + week[now.getDay()] + ')';          // 23-03-23(목)
+        const timeStr = myDatetime(now).substring(11, 19);  // 10:16:00
+        document.getElementById('date').innerHTML = dateStr;
         
-    })
-})
+        document.getElementById('h1').src = `digits/${timeStr[0]}.svg`;
+        document.getElementById('h2').src = `digits/${timeStr[1]}.svg`;
+        document.getElementById('m1').src = `digits/${timeStr[3]}.svg`;
+        document.getElementById('m2').src = `digits/${timeStr[4]}.svg`;
+        document.getElementById('s1').src = `digits/${timeStr[6]}.svg`;
+        document.getElementById('s2').src = `digits/${timeStr[7]}.svg`;
+    }, 1000);
+}
