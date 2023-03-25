178a5c2cf69fbc65ef88f282f544379701fb81e8
Mayhem-XD <leahblo1994@gmail.com>
2023-03-25 22:24:40 +0900

    19과제_수정중

--- 02.JavaScript/ch14jquery/19.과제.js
+++ 02.JavaScript/ch14jquery/19.과제.js
@@ -1,28 +1,5 @@
 $(document).ready(function()
 {
-    // Define the images array
-    var images = 
-    [
-    "../../00.numbers_img/1.svg",
-    "../../00.numbers_img/2.svg",
-    "../../00.numbers_img/3.svg",
-    "../../00.numbers_img/4.svg",
-    "../../00.numbers_img/5.svg"
-    ];
-
-    // Loop through the images array and create carousel items
-    $.each(images, function(index, value)
-    {
-        var item = $('<div class="carousel-item"><img src="' + value + '" alt="img' + (index+1) + '" class="d-block" style="width:100%"></div>');
-        if(index == 0)
-        {
-            item.addClass('active');
-        }
-        $('#myCarousel .carousel-inner').append(item);
-        // Activate the carousel
-        $('#myCarousel').carousel();
-    });
-
     var slideCount = $('.carousel-inner').children().length;
     for (var i = 0; i < slideCount; i++) 
     {
